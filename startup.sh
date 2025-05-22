b51fb96e6e8345a556241345547d8cd0037eb4e4
KingClarke <D00267783@student.dkit.ie>
2025-05-22 11:36:45 +0100

    Refactor startup script to build frontend before serving Laravel

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd /home/site/wwwroot
 
-# Start nginx with your custom config in the foreground (blocks and keeps container alive)
-nginx -c /home/site/wwwroot/nginx.conf -g "daemon off;"
+# Install and build frontend
+npm install
+npm run build
+
+# Serve Laravel
+php artisan serve --host 0.0.0.0 --port 8080
