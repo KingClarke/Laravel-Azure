f216367a11cd4531e879fbd896e664b8c1e6a525
KingClarke <D00267783@student.dkit.ie>
2025-05-22 11:07:06 +0100

    Refactor nginx and startup scripts for improved clarity and structure

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Start PHP-FPM in the background
-php-fpm &
-
 # Start nginx with your custom config in the foreground (blocks and keeps container alive)
 nginx -c /home/site/wwwroot/nginx.conf -g "daemon off;"
